f203a23502a9afef7f8e57a704d503ec7e40f16a
sonali-kumari1505 <sonalisinghrajput1498@gmail.com>
2026-07-18 08:11:34 +0530

    initialized fronted

---
+++ fronted/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Button } from './components/ui/button'
+
+const App = () => {
+  return (
+    <div>
+      <Button> button</Button>
+    </div>
+  )
+}
+
+export default App
+
+
+
